999269778cb07c5bbf0389ca8a62eb733e840e1d
Soares Chen <soares.chen@gmail.com>
2015-09-10 00:29:02 +0800

    Properly export immutable map and helpers.

--- src/lib/immutable/index.js
+++ src/lib/immutable/index.js
@@ -1,1 +1,7 @@
-export { Map as ImmutableMap } from 'immutable'
+import immutable from 'immutable'
+
+export const immutableMap = immutable.Map
+export const ImmutableMap = immutable.Map
+
+export const isImmutableMap = map =>
+  ImmutableMap.isMap(map)
