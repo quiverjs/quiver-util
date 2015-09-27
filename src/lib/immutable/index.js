500faa3f4d985c6d5c5b0bf1f6409529c8e3565c
Soares Chen <soares.chen@gmail.com>
2015-09-27 15:52:27 +0800

    Add ImmutableList link from immutable.js

--- src/lib/immutable/index.js
+++ src/lib/immutable/index.js
@@ -3,5 +3,11 @@ import immutable from 'immutable'
 export const immutableMap = immutable.Map
 export const ImmutableMap = immutable.Map
 
+export const immutableList = immutable.List
+export const ImmutableList = immutable.List
+
 export const isImmutableMap = map =>
   ImmutableMap.isMap(map)
+
+export const isImmutableList = map =>
+  ImmutableList.isList(map)
